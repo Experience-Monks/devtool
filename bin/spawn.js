fab4defc438dd9c9a3c27b7c23fe8d190405c370
mattdesl <dave.des@gmail.com>
2016-01-29 11:44:49 -0500

    remove Chromium/Electron logs on OSX from stderr

--- bin/spawn.js
+++ bin/spawn.js
@@ -1,17 +1,25 @@
 // spawns devtool with given CLI arguments
 const spawn = require('child_process').spawn;
 const electron = require('electron-prebuilt');
+const through = require('through2');
 const path = require('path');
 const serverPath = path.resolve(__dirname, '..', 'server.js');
 
 module.exports = spawnDevtool;
 function spawnDevtool (args) {
+  var helperRegex = /[\d\-\s\:\.]+\sElectron\sHelper\[[\d\:\.]+\]/;
   var serverArgs = [ serverPath ].concat(args);
   var proc = spawn(electron, serverArgs, {
     stdio: [ process.stdin, 'pipe', 'pipe' ]
   });
   proc.stdout.pipe(process.stdout);
-  proc.stderr.pipe(process.stderr);
+  proc.stderr.pipe(through(function (data, enc, next) {
+    if (!helperRegex.test(data.toString())) {
+      // Hide some Chromium/Electron logs on OSX
+      this.push(data);
+    }
+    next();
+  })).pipe(process.stderr);
 
   proc.on('close', function (code) {
     process.exit(code);
