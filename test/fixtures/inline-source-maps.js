a3e2b89de0e50f6ce1a63ec8a35e7e4680a6fc3f
mattdesl <dave.des@gmail.com>
2016-05-02 12:05:47 -0400

    fix source maps, inline and external, and add option to disable source maps

--- test/fixtures/inline-source-maps.js
+++ test/fixtures/inline-source-maps.js
@@ -1,4 +1,7 @@
+
 var withBase64 = require('./source-map-base64.js');
 var withMap = require('./source-map-external.js');
 console.log(withBase64());
 console.log(withMap.default());
+
+console.log("Hello")
